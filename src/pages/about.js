94f31c73711013ab7e0238c31c78da84dc63cfb6
devinmerch <devinmerch@gmail.com>
2023-11-13 16:12:29 -0500

    about us page

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,7 @@ const About = () => {
 
       {/* Header Section */}
       <div className="bg-gray-900 py-12 mb-6 text-center border border-slate-800 text-white">
-        <h1 className="text-4xl font-bold mb-3">A Little Bit About Us</h1>
+        <h1 className="text-4xl font-bold mb-3">A Little Bit About Us.</h1>
         <p className="text-lg max-w-xl mx-auto">
         BiblioSwap is a web application that will allow users to upload their existing library of books they are interested in trading for new books. 
         </p>
